2123d2bfe03c11d301b99b9c2d7939778b5ad37b
Gaël <gael+git@demette.fr>
2018-11-30 15:46:29 +0100

    Create main page

---
+++ components/form/field.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    :class="{'is-horizontal': isHorizontal}"
+    class="field">
+    <div
+      v-if="label"
+      class="field-label is-normal">
+      <label
+        :for="fieldId"
+        class="label">{{ label }}</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <p class="control">
+          <float-label>
+            <slot />
+          </float-label>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import floatLabel from 'vue-float-label/components/FloatLabel';
+
+export default {
+  components: {
+    floatLabel,
+  },
+  props: {
+    isHorizontal: {
+      type: Boolean,
+      default: true,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+    fieldId: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import "~assets/css/head";
+.control {
+  margin-top: 1.3em;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container has-text-centered">
+    <img
+      src="~/static/logo.svg"
+      alt="E-chauffeur">
+    <form @submit.prevent="loadRide(rideId)">
+      <ec-field>
+        <input
+          v-model="rideId"
+          class="input"
+          type="text"
+          placeholder="Identifiant de course">
+      </ec-field>
+      <button
+        :disabled="!rideId"
+        class="button"
+        type="submit">Voir la course</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import ecField from '~/components/form/field.vue';
+
+export default {
+  components: {
+    ecField,
+  },
+  data: () => ({ rideId: null }),
+  methods: {
+    loadRide(rideId) {
+      this.$router.push({ name: 'ride_id', params: { ride_id: rideId } });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/css/head";
+.container {
+  max-width: 500px;
+  padding: $size-1;
+}
+</style>
