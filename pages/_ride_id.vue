658910b6ec32a633dd5698885572f75ddc068db9
Gaël <gael+git@demette.fr>
2018-11-30 15:47:38 +0100

    Create ride page

---
+++ pages/_ride_id.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <section class="map">
+      <no-ssr>
+        <l-map
+          :zoom="13"
+          :center="[47.413220, -1.219482]"
+        >
+          <l-tile-layer url="//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <l-marker :lat-lng="[47.413220, -1.219482]" />
+        </l-map>
+      </no-ssr>
+    </section>
+    <section class="elements">
+      {{ rideId }}
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  asyncData: ({ params: { ride_id: rideId } }) => ({
+    rideId,
+  }),
+};
+</script>
+
+<style scoped lang="scss">
+  .map {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    z-index: 0;
+  }
+  .elements {
+    z-index: 1;
+    position: relative;
+    color: red;
+  }
+</style>
