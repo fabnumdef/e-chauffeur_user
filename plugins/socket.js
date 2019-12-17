4b99faaa5bb42a0c21f98b16edaf6989e658ae0b
Charles Capelli <charles.capelli@qonfucius.team>
2019-12-17 16:42:42 +0100

    Fixed socket bug with Lib-vue bump

--- plugins/socket.js
+++ plugins/socket.js
@@ -3,24 +3,27 @@ import { CANCELED_STATUSES } from '@fabnumdef/e-chauffeur_lib-vue/api/status';
 
 export default function ({ store, app }) {
   const ioInstance = app.$io;
-  ioInstance.on('connect', () => {
-    ioInstance.emit(
-      'roomJoinRide',
-      { id: store.getters['ride/ride'].id, token: store.getters['ride/ride'].token },
-    );
-  });
-  ioInstance.on('rideUpdate', (ride) => {
-    if (ride && (ride.status === DELIVERED || CANCELED_STATUSES.indexOf(ride.status) !== -1)) {
-      ioInstance.close();
-    }
-  });
-  const autoConnect = (ride) => {
-    if (ride && ride.id && ride.status !== DELIVERED && CANCELED_STATUSES.indexOf(ride.status) === -1) {
-      ioInstance.open();
-    } else {
-      ioInstance.close();
-    }
-  };
-  autoConnect(store.getters['ride/ride']);
-  store.watch((state, getters) => getters['ride/ride'], autoConnect);
+  if (ioInstance) {
+    ioInstance.on('connect', () => {
+      ioInstance.emit(
+        'roomJoinRide',
+        { id: store.getters['ride/ride'].id, token: store.getters['ride/ride'].token },
+      );
+    });
+    ioInstance.on('rideUpdate', (ride) => {
+      if (ride && (ride.status === DELIVERED || CANCELED_STATUSES.indexOf(ride.status) !== -1)) {
+        ioInstance.close();
+      }
+    });
+
+    const autoConnect = (ride) => {
+      if (ride && ride.id && ride.status !== DELIVERED && CANCELED_STATUSES.indexOf(ride.status) === -1) {
+        ioInstance.open();
+      } else {
+        ioInstance.close();
+      }
+    };
+    autoConnect(store.getters['ride/ride']);
+    store.watch((state, getters) => getters['ride/ride'], autoConnect);
+  }
 }
