e44d4ef8bbaec0403740db86abe1215e593e70dd
Charles Capelli <charles.capelli@qonfucius.team>
2019-12-18 13:25:22 +0100

    Clean fix to socket bug while following driver position in user app

--- store/driver.js
+++ store/driver.js
@@ -9,27 +9,8 @@ export const state = () => ({
 export const getters = {
   position: (s) => s.position,
   carPosition: (s) => {
-    if (
-      s.position
-      && s.position.coordinates
-    ) {
-      /*
-       * Error report :
-       * s.position.coordinates is an array composed of lon & lat fetched in asyncData
-       * Websockets send back new datas when driver's position update
-       * Data is sent with correct format by API but
-       * when it arrives in this function, it becomes an object : s.position.coordinates.coordinates
-       *
-       * Here is a dummy fix to make the magic but it's not the cleanest one.
-       */
-      if (s.position.coordinates.coordinates) {
-        const [lon, lat] = s.position.coordinates.coordinates;
-        return [lat, lon];
-      }
-      const [lon, lat] = s.position.coordinates;
-      return [lat, lon];
-    }
-    return null;
+    const [lon, lat] = s.position.coordinates;
+    return [lat, lon];
   },
 };
 
